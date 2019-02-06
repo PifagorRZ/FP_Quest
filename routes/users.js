7212e3d90d69eebf4d28a52c34889fec8cbd9497
Pifagor <zheka.rachkovan@gmail.com>
2019-02-06 23:48:49 +0300

    log-in4

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-
+var db;
 
 MongoClient.connect('mongodb+srv://freddy2010:qweQWE333@list-0ixqy.mongodb.net/test?retryWrites=true', (err, database) => {
-    var db = database.db('FP_Quest');
+    db = database.db('FP_Quest');
 })
 /* GET users listing. */
 router.get('/', function(req, res, next) {
