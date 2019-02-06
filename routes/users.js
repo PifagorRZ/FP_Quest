6c5a7896b10d5e36290e13e1f45c9547edb0ef09
Pifagor <zheka.rachkovan@gmail.com>
2019-02-06 23:45:41 +0300

    log-in3

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
 
+
+MongoClient.connect('mongodb+srv://freddy2010:qweQWE333@list-0ixqy.mongodb.net/test?retryWrites=true', (err, database) => {
+    var db = database.db('FP_Quest');
+})
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,7 +13,13 @@ router.get('/', function(req, res, next) {
 
 router.put('/sign-in', (req, res) => {
   console.log('autorized')
-
+    db.collection('Users').updateOne({}, {$inc: {"activeUsers": 1}})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 module.exports = router;
